ae837ee1ee9bee73e2fd910d14110b1ae73912fe
AustinBH <harlowaustin2@gmail.com>
2019-09-06 11:50:58 -0700

    added initial user choice, still need to add handling for when user inputs something that is not a number

--- tic_tac_toe/tic_tac_toe_v1.py
+++ tic_tac_toe/tic_tac_toe_v1.py
@@ -3,9 +3,9 @@ import random
 player = ''
 # Creating empty board to hold 3 rows
 board = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' ']
 ]
 # Creating example board to display where choices go
 example_board = [
@@ -25,8 +25,24 @@ def display_board(board):
     print(f' {board[1][0]} | {board[1][1]} | {board[1][2]} ')
     print('-----------')
     print(f' {board[2][0]} | {board[2][1]} | {board[2][2]} ')
+# This function will allow the user to choose their move
+def get_user_choice():
+    global board
+    global player
+    choice = int(input('What is your move?\n'))
+    # Need to add handling for if a user inputs a string that cannot be converted to int
+    if choice >= 1 and choice <= 3:
+        board[0][choice - 1] = player
+    elif choice >= 4 and choice <= 6:
+        board[1][choice%3 - 1] = player
+    elif choice >= 7 and choice <= 9:
+        board[2][choice%3 - 1] = player
+    else:
+        print('Choose a valid move')
+        get_user_choice()
+    display_board(board)
     
 welcome()
 print('Here is an example of the Tic Tac Toe Board:')
 display_board(example_board)
-display_board(board)
+get_user_choice()
