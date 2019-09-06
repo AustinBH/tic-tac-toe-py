23b6942e83707f7e58f25d9448d922a49f78a0e0
AustinBH <harlowaustin2@gmail.com>
2019-09-06 11:06:45 -0700

    added a few started functions still need to figure out correct logic for displaying board

--- tic_tac_toe/tic_tac_toe_v1.py
+++ tic_tac_toe/tic_tac_toe_v1.py
@@ -1,1 +1,31 @@
 import random
+
+player = ''
+# Creating empty board to hold 3 rows
+board = [
+    ['X', 'O'],
+    [],
+    []
+]
+# Creating initial player team selection and starting welcome message
+def welcome():
+    global player
+    print('Welcome to Tic Tac Toe!')
+    player = input('Select your team (X or O): ').upper()
+# Helper method for our board method to return 
+def display_index(row, col):
+    global board
+    # Need to figure out the correct logic to make sure that we dont throw error
+    # Still W.I.P
+    if board[row] and board[row][col]:
+        return board[row][col]
+    return ''
+
+def display_board():
+    print(f' {display_index(0,0)} | {display_index(0,1)} | {display_index(0,2)} ')
+    print(f' {display_index(1,0)} | {display_index(1,1)} | {display_index(1,2)} ')
+    print(f' {display_index(2,0)} | {display_index(2,1)} | {display_index(2,2)} ')
+
+
+
+display_board()
